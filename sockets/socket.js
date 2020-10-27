2c20be87b4e8031c9f67015447c89c49137af96f
Omar Lerma <leooma24@gmail.com>
2020-10-26 19:23:57 -0700

    App Terminada

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,17 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+bands.addBand( new Band('Queen') );
+bands.addBand( new Band('Bon Jovi') );
+bands.addBand( new Band('Héroes del Silencio') );
+bands.addBand( new Band('Metallica') );
+
 // Sockets
 io.on('connection', client => {
     console.log('cliente conectado');
+    client.emit('active-bands', bands.getBands() );
 
     client.on('mensaje', (payload) => {
         console.log(payload)
@@ -9,9 +19,27 @@ io.on('connection', client => {
         io.emit('mensaje', { admin: 'Nuevo mensaje' });
     })
 
+    client.on('disconnect', () => {
+        console.log('cliente desconectado');
+    });
 
+    client.on('emitir-mensaje', ( payload ) => {
+        console.log(payload)
+        client.broadcast.emit('nuevo-mensaje', payload);
+    });
 
-   client.on('disconnect', () => {
-       console.log('cliente desconectado');
-   });
+    client.on('vote-band', (payload) => {
+        bands.voteBand( payload.id )
+        io.emit('active-bands', bands.getBands() );
+    });
+
+    client.on('add-band', (payload) => {
+        bands.addBand( new Band( payload.name ) );
+        io.emit('active-bands', bands.getBands() );
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
+    });
 });
